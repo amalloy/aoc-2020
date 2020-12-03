67741e0c97eabcde94e2b95955517153eb485209
Alan Malloy <alan@malloys.org>
2020-12-03 02:01:21 -0800

    Day 3, part 2

--- day03/src/Main.hs
+++ day03/src/Main.hs
@@ -6,19 +6,34 @@ import Data.Set (Set)
 import qualified Data.Set as S
 
 type Input = (Int, [Set Int])
+data Slope = Slope {right, down :: Int}
+
+takeNth :: Int -> [a] -> [a]
+takeNth _ [] = []
+takeNth n xs@(x:_) = x : takeNth n (drop n xs)
 
 hitTree :: Int -> Int -> Set Int -> Bool
 hitTree width x = S.member (x `mod` width)
 
-part1 :: Input -> Int
-part1 (width, trees) =
+treeCount :: Input -> Slope -> Int
+treeCount (width, forest) (Slope r d) =
   length
   . filter (uncurry (hitTree width))
-  . zip [0, 3..]
-  $ trees
+  . zip [0, r..]
+  . takeNth d
+  $ forest
 
-part2 :: Input -> ()
-part2 i = ()
+part1 :: Input -> Int
+part1 = (`treeCount` (Slope 3 1))
+
+
+part2 :: Input -> Int
+part2 i = product . map (treeCount i) $ [ Slope 1 1
+                                        , Slope 3 1
+                                        , Slope 5 1
+                                        , Slope 7 1
+                                        , Slope 1 2
+                                        ]
 
 prepare :: String -> Input
 prepare = (length . head &&& map readTrees) . lines
