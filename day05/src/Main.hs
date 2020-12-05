0ea522ce88ad7a72ef29591b739ea6daded8e060
Alan Malloy <alan@malloys.org>
2020-12-04 21:37:21 -0800

    Day 5, part 2

--- day05/src/Main.hs
+++ day05/src/Main.hs
@@ -4,21 +4,22 @@ module Main where
 
 import Control.Arrow ((&&&))
 import Data.Bool (bool)
+import Data.List (sort)
 
-type Input = [String]
+type Input = [Int]
 
 asBinary :: (a -> Bool) -> [a] -> Int
 asBinary one = foldl (\n x -> 2 * n + digit x) 0
   where digit = bool 0 1 . one
 
 part1 :: Input -> Int
-part1 = maximum . map (asBinary (`elem` "BR"))
+part1 = maximum
 
-part2 :: Input -> ()
-part2 i = ()
+part2 :: Input -> [Int]
+part2 = map (succ . fst) . filter (\(x, y) -> y - x == 2) . (zip <*> tail) . sort
 
 prepare :: String -> Input
-prepare = lines
+prepare = map (asBinary (`elem` "BR")) . lines
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
