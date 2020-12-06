99325200897c36c45eab941bb67a0b54ec7dd471
Alan Malloy <alan@malloys.org>
2020-12-05 21:13:47 -0800

    Day 6, part 2

--- day06/src/Main.hs
+++ day06/src/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Control.Arrow ((&&&))
 import qualified Data.Set as S
-import Data.List (foldl')
+import Data.List (foldl', foldl1')
 import Data.List.Split (splitOn)
 
 type Input = [[S.Set Char]]
@@ -10,8 +10,8 @@ type Input = [[S.Set Char]]
 part1 :: Input -> Int
 part1 = sum . map (length . foldl' S.union S.empty)
 
-part2 :: Input -> ()
-part2 = const ()
+part2 :: Input -> Int
+part2 = sum . map (length . foldl1' S.intersection)
 
 prepare :: String -> Input
 prepare = map (map S.fromList . lines) . splitOn "\n\n" . init
